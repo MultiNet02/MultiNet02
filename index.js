91e27afc7a9fd9ccb869a4767d98d5ff9b04bceb
Stephen Guo <109275192+MultiNet02@users.noreply.github.com>
2024-02-12 22:44:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,18 @@ const readme = `\
 
 ---
 
-⏰ Updated on ${new Date().toUTCString()}
+<div align="center"> 
+</div>
+   <a href="https://github.com/MultiNet02" target="_blank"><img alt="MultiNet02" src="https://badges.pufler.dev/visits/MultiNet02/MultiNet02?logo=GitHub&label=visits&color=success&logoColor=white&style=flat-square"/></a>  
+</div>
+
+- 🕵️ My research interests include but not limited to: Graph Neural Network, Embodied AI, SLAM, Muiltimodal LLM, Quantitative Trading.
+- 🦸 I am also passionate about reading about cognitive science, memory mechanisms and philosophy, especially Schopenhauer and Deleuze.
+- 👀 If you are interested in languages learning, Viaja a través de paisajes or Viel Spaß beim Lesen von Gedichten，feel free to join in [Slack Team](). Let's make a soul mate be found possible in our short lifetime.
+- 🌐 Checkout [my homepage](http://multinet02.github.io/) and [Google Scholar]() for more information.
+
+![](https://github-readme-stats.vercel.app/api?username=MultiNet02&show_icons=true&theme=synthwave)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=MultiNet02&layout=compact)](https://github.com/anuraghazra/github-readme-stats)
 
 \
 `
